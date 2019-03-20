db4099cd7a8eb31b57734507aacefab3babd2096
YangFeng <250970437@qq.com>
2019-03-20 09:29:08 +0800

    Create update.sh
    Add update shell script.

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "${1}"
+url="http://39.104.105.27:8181/cloudapp/v2/applications/update/${1}"
+HTTP_CODE=`curl -X GET -o /dev/null -s --head -w "%{http_code}" "${url}"`
+if [ ${HTTP_CODE} -ne 200 ]
+then
+ echo -e "error-${HTTP_CODE}"
+ exit 1
+else 
+ echo -e "success-${HTTP_CODE}"
+ exit 0
+fi
